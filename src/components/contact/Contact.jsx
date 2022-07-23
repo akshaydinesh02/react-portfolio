45c9b1521cdeb6a4c781b30f1e7529d4466ff7da
Akshay Dinesh <akshay5632@gmail.com>
2022-07-23 22:37:07 +0530

    landing page update
    - added and designed new component - (contact)

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,8 +1,42 @@
 import React from "react";
 import "./contact.css";
+import Phone from "../../img/phone.png";
+import Email from "../../img/email.png";
+import Address from "../../img/address.png";
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <div className="c">
+      <div className="c-bg"></div>
+      <div className="c-wrapper">
+        <div className="c-left">
+          <h1 className="c-title">Let's get in touch!</h1>
+          <div className="c-info">
+            <div className="c-info-item">
+              <img src={Phone} alt="" className="c-icon" /> +91 123456789
+            </div>
+            <div className="c-info-item">
+              <img src={Email} alt="" className="c-icon" /> akshay5632@gmail.com
+            </div>
+            <div className="c-info-item">
+              <img src={Address} alt="" className="c-icon" /> Bengaluru,
+              Karnataka
+            </div>
+          </div>
+        </div>
+        <div className="c-right">
+          <p className="c-desc">Drop a message!</p>
+          <form>
+            <input type="text" placeholder="Name" name="user_name" />
+            <input type="text" placeholder="Subject" name="user_subject" />
+            <input type="text" placeholder="Email" name="user_email" />
+            <textarea rows="5" placeholder="Message" name="message" />
+            <button>Submit</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
